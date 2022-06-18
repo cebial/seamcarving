73ec1991aabb8c0de49a29b138e1a22470bcd44b
cebial <biglazypanda@gmail.com>
2022-06-18 16:24:14 +0200

    Stage 4/6: Find the seam, first working version

--- Main.kt
+++ Main.kt
@@ -4,14 +4,15 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import kotlin.math.max
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class SeamCarver(srcName: String, private val destName: String) {
     private val src = ImageIO.read(File(srcName))
     private val energy = Array(src.height) { Array(src.width) { 0.0 } }
+    private val energyDP = Array(src.height) { Array(src.width) { 0.0 } }
     private var maxEnergy = 0.0
+    private val currentSeam = Array(src.height) { 0 }
 
     private fun calculateEnergy() {
         val xMax = src.width - 2
@@ -23,12 +24,59 @@ class SeamCarver(srcName: String, private val destName: String) {
                 val dY = color(x, y.coerceIn(1, yMax) - 1) - color(x, y.coerceIn(1, yMax) + 1)
                 energy[y][x] = sqrt(dX + dY)
 
-                maxEnergy = max(energy[y][x], maxEnergy)
+                maxEnergy = maxOf(energy[y][x], maxEnergy)
             }
         }
     }
 
-    private fun writeImage() {
+    private fun findVerticalSeam() {
+        // energy values for the first row are identical to the original values
+        energyDP[0] = energy[0]
+
+        // for all other rows, for each pixel, new energy is equal to its own energy
+        // plus the minimum of the three above
+        for (y in 1 until src.height) {
+            for (x in 0 until src.width) {
+                energyDP[y][x] = energy[y][x] + when (x) {
+                    0 -> minOf(energyDP[y - 1][x], energyDP[y - 1][x + 1])
+                    src.width - 1 -> minOf(energyDP[y - 1][x - 1], energyDP[y - 1][x])
+                    else -> minOf(energyDP[y - 1][x - 1], energyDP[y - 1][x], energyDP[y - 1][x + 1])
+                }
+            }
+        }
+
+        var x = energyDP[src.height - 1].indexOf(energyDP[src.height - 1].minOf { it })
+
+        currentSeam[src.height - 1] = x
+
+        for (y in src.height - 2 downTo 0) {
+            val map = mutableMapOf<Int, Double>()
+            if (x > 0) map[x - 1] = energyDP[y][x - 1]
+            map[x] = energyDP[y][x]
+            if (x < src.width - 1) map[x + 1] = energyDP[y][x + 1]
+
+            currentSeam[y] = map.minByOrNull { it.value }!!.key
+            x = currentSeam[y]
+        }
+        currentSeam.forEach(::println)
+
+    }
+
+    private fun writeImageWithSeam() {
+        val dest = BufferedImage(src.width, src.height, BufferedImage.TYPE_INT_RGB)
+
+        for (x in 0 until src.width) {
+            for (y in 0 until src.height) {
+                val col = if (currentSeam[y] == x) Color.RED else Color(src.getRGB(x, y))
+                dest.setRGB(x, y, col.rgb)
+            }
+        }
+
+
+        ImageIO.write(dest, "png", File(destName))
+    }
+
+    private fun writeImageWithIntensity() {
         val dest = BufferedImage(src.width, src.height, BufferedImage.TYPE_INT_RGB)
 
         for (x in 0 until src.width) {
@@ -48,7 +96,8 @@ class SeamCarver(srcName: String, private val destName: String) {
 
     fun run() {
         calculateEnergy()
-        writeImage()
+        findVerticalSeam()
+        writeImageWithSeam()
     }
 }
 
